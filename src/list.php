86c5fc2b3c60fc91b24ac23b19fd3ea70641aea1
djjlsajfiajf <2201176@s.asojuku.ac.jp>
2024-01-26 23:52:25 +0900

    l

---
+++ src/list.php
@@ -0,0 +1,23 @@
+<?php
+    const SERVER ='mysql214.phy.lolipop.lan';
+    const DBNAME ='LAA1517433-shop';
+    const USER = 'LAA1517433';
+    const PASS = 'Pass0530';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
+<table>
+<tr>めんどくさいこと<th></th><th>ID</th></tr>
+<?php
+    $pdo=new PDO($connect, USER, PASS);
+
+	foreach ($pdo->query('select * from bothered') as $row) {
+        echo '<tr>';
+		echo '<td>', $row['name'],'</td>' ;
+		echo '<td>', $row['ID'],'</td>';
+        echo '</tr>';
+		echo "\n";
+	}
+?>
+</table>
+<button onclick="location.href='index.html'">戻る</button>
