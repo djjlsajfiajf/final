bbdc372da6e685395201ee83ed817cac4beb0625
djjlsajfiajf <2201176@s.asojuku.ac.jp>
2024-01-26 23:27:35 +0900

    ｓ

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>b</b></title>
+</head>
+<body>
+    <h1>めんどくさいこと管理メニュー</h1>
+    <form action="branch.php">
+    <button type="submit" name="select">一覧</button>   
+    <button type="submit" name="insert">登録</button>
+    <button type="submit" name="update">更新</button>
+    <button type="submit" name="delete">削除</button> 
+    </form>
+</body>
+</html>
