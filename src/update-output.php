a3476e082293e636d67279fc02add596753ff5a7
djjlsajfiajf <2201176@s.asojuku.ac.jp>
2024-01-27 00:44:23 +0900

    e

--- src/update-output.php
+++ src/update-output.php
@@ -1,32 +1,33 @@
 <?php
-    const SERVER ='mysql214.phy.lolipop.lan';
-    const DBNAME ='LAA1517433-shop';
-    const USER = 'LAA1517433';
-    const PASS = 'Pass0530';
+const SERVER ='mysql214.phy.lolipop.lan';
+const DBNAME ='LAA1517433-shop';
+const USER = 'LAA1517433';
+const PASS = 'Pass0530';
 
-    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+$connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
 ?>
 
 <?php
-    $pdo=new PDO($connect, USER, PASS);
-    $spl=$pdo->prepare('update bothered set ID=?,name=?');
-    if (empty($_REQUEST['name'])) {
-        echo 'ブランド名を入力してください。';
-    } else
+$pdo = new PDO($connect, USER, PASS);
+$sql = $pdo->prepare('UPDATE bothered SET name=? WHERE ID=?');
 
-    if($spl->execute([htmlspecialchars($_POST['name']),$_POST['ID']])){
+if (empty($_POST['name'])) {
+    echo 'ブランド名を入力してください。';
+} else {
+    
+    if ($sql->execute([$_POST['name'], $_POST['ID']])) {
         echo '更新に成功しました。';
-    }else{
+    } else {
         echo '更新に失敗しました。';
     }
-    
+}
 ?>
-        <hr>
-        <table>
-        <tr><th>ID</th><th>めんどくさいこと</th></tr>
+<hr>
+<table>
+    <tr><th>ID</th><th>めんどくさいこと</th></tr>
 
 <?php
-foreach ($pdo->query('select * from bothered') as $row) {
+foreach ($pdo->query('SELECT * FROM bothered') as $row) {
     echo '<tr>';
     echo '<td>', $row['ID'], '</td>';
     echo '<td>', $row['name'], '</td>';
@@ -34,5 +35,5 @@ foreach ($pdo->query('select * from bothered') as $row) {
     echo "\n";
 }
 ?>
-        </table>
+</table>
 <button onclick="location.href='update.php'">更新画面へ戻る</button>
